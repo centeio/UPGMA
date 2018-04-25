a9067f9e958ec94ee7111534b2fe8814c21751b0
Higor Araújo dos Anjos <higor.araujo.anjos@gmail.com>
2018-04-25 01:08:44 +0100

    Calculating distances and displaying

--- upgma.py
+++ upgma.py
@@ -1,35 +1,54 @@
 from matrices import *
+from Bio import pairwise2
+from Bio import Phylo
+from Bio.Phylo.TreeConstruction import DistanceCalculator
+from io import StringIO
+from util.io import *
 import sys
 import itertools
 
-matrix_dict =   { 'BLOSUM62': BLOSUM62, 'DNAFULL': DNAfull, 'PAM250': PAM250}
-matrix = matrix_dict['BLOSUM62']
+DEBUG = True
 
 class Tree(object):
-    def __init__(self, left = None, right = None, data = None, weight = None, height = None):
+    def __init__(self, left = None, right = None, data = None, height = None):
         self.left = left
         self.right = right
         self.data = data
-        self.weight = weight
         self.height = height
     
     def __repr__(self):
         #return "data: %s, weight: %s, height: %s" % (self.data, self.weight, self.height)
         return "%s" % (self.data)
 
+
+def toNewick(tree):
+    
+    if(tree is None):
+        return ''
+    
+    if(tree.left is None and tree.right is None):
+        return str(tree.data)
+        
+    return "("+toNewick(tree.left)+")("+toNewick(tree.right)+")"
+    
+aux = {}
+
 def d(ci, cj):
-#    if len(ci.data) == 1:
-#        if len(cj.data) == 1: #both have one letter
-#            return matrix[ci.data][cj.data]
-#        return (d(ci, cj.right) + d(ci, cj.left)) / 2 #only ci has one letter
-#    elif len(cj.data) == 1
-#        return (d(ci.right, cj) + d(ci.right, cj)) / 2 # only cj has one letter
+    calc = DistanceCalculator(model='blosum62')
+    
+    alignment = pairwise2.align.globalxx(ci.data,cj.data)[0]
 
-#    return d(ci.right, cj.) + d(ci.right, cj)) / 2 
-    if len(ci.data) == len(cj.data) == 1: #both have one letter
-        return matrix[ci.data][cj.data]
-    return (ci.weight + cj.weight) / 2
+    seq1 = alignment[0]
+    seq2 = alignment[1]
+    
+    #An improvement for preventing re-calculating
+    try:
+        dist = aux[(seq1,seq2)]
+    except KeyError as ex:
+        aux[(seq1,seq2)] = calc._pairwise(seq1, seq2)
+        dist = aux[(seq1,seq2)]
 
+    return dist
 
 def buildtree(seq1):
     clusters = []
@@ -38,48 +57,30 @@ def buildtree(seq1):
     #definir uma folha para cada sequencia e colocar na altura 0
 
     for i in range(0, len(seq1)):
-        clusters.append(Tree(data = seq1[i], height = 0, weight = 0))
-
-
+        clusters.append(Tree(data = seq1[i], height = 0))
+    if(DEBUG):
+        print(clusters)
     #enquanto ha mais que 2 clusters
     min_dij = sys.maxsize
     ci = Tree()
     cj = Tree()
-    print(clusters)
 
     while (len(clusters) >= 2):
         comb = list(itertools.combinations(clusters, 2))
         l,r = min(comb,key=lambda e: d(e[0],e[1]))
         clusters.remove(l)
         clusters.remove(r)
-        clusters.append(Tree(l, r, l.data + r.data, 1,d(l,r)/2))
-        print(clusters)
-
-       # print("current: %s, min: %s" % (current.data, x.data))
-#      clusters.append(Tree(current, m, current.data + x.data, d(m,current)))
- #       for i in range(0, len(clusters)):
-  #          for j in range(i+1, len(clusters)):
-   #             if (d(clusters[i], clusters[j]) < min_dij):
-    #                ci = clusters[i]
-     #               cj = clusters[j]
-      #              min_dij = d(ci, cj) 
-
-
-
-    #determinar dois clusters i e j, com a menor dij
-    #definir novo cluster Ck com Clusters i e j e criar nos na altura dij/2
-    #substituir clusters i e j por Ck
-
-    #dado um novo cluster ck podemos calcular a sua distancia a todos os outros
-
-def print_tree:
-
+        clusters.append(Tree(l, r, l.data + r.data,d(l,r)/2))
+        if(DEBUG):
+            print(clusters)
     
+    return clusters[0]
 
 if __name__ == '__main__':
-    seq1 = "AAPGE"
-
-    buildtree(seq1)
-
-
-
+    #seq1 = read_fasta(read_file('inputs/Q61743.fasta'))
+    seq1 = "MLSRKGIIPEEYVLTRLAEDPAEPRY"
+    result = buildtree(seq1)
+    string = toNewick(result)
+    tree = Phylo.read(StringIO(string+';'),"newick")
+    Phylo.draw(tree)
+    
