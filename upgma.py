ac3d06a24c37a9ac04bc7f0bb566aaf74887f6b0
Centeio <c.centeiojorge@outlook.pt>
2018-04-25 17:53:15 +0100

    heap version

--- upgma.py
+++ upgma.py
@@ -1,6 +1,7 @@
 from matrices import *
 import sys
 import itertools
+from heapq import heappush, heappop
 
 matrix_dict =   { 'BLOSUM62': BLOSUM62, 'DNAFULL': DNAfull, 'PAM250': PAM250}
 matrix = matrix_dict['BLOSUM62']
@@ -12,6 +13,16 @@ class Tree(object):
         self.data = data
         self.weight = weight
         self.height = height
+
+    def __lt__(self, other):
+        return self.height * 2 < other.height * 2
+
+    def __eq__(self, other):
+        if len(self.data) == 1 or len(other.data) == 1:
+            return self.data == other.data
+
+        return self.left == other.left and self.right == self.right 
+             
     
     def __repr__(self):
         #return "data: %s, weight: %s, height: %s" % (self.data, self.weight, self.height)
@@ -40,14 +51,11 @@ def buildtree(seq1):
     for i in range(0, len(seq1)):
         clusters.append(Tree(data = seq1[i], height = 0, weight = 0))
 
-
     #enquanto ha mais que 2 clusters
-    min_dij = sys.maxsize
-    ci = Tree()
-    cj = Tree()
+
     print(clusters)
 
-    while (len(clusters) >= 2):
+    while (len(clusters) > 2):
         comb = list(itertools.combinations(clusters, 2))
         l,r = min(comb,key=lambda e: d(e[0],e[1]))
         clusters.remove(l)
@@ -55,31 +63,73 @@ def buildtree(seq1):
         clusters.append(Tree(l, r, l.data + r.data, 1,d(l,r)/2))
         print(clusters)
 
-       # print("current: %s, min: %s" % (current.data, x.data))
-#      clusters.append(Tree(current, m, current.data + x.data, d(m,current)))
- #       for i in range(0, len(clusters)):
-  #          for j in range(i+1, len(clusters)):
-   #             if (d(clusters[i], clusters[j]) < min_dij):
-    #                ci = clusters[i]
-     #               cj = clusters[j]
-      #              min_dij = d(ci, cj) 
-
-
-
     #determinar dois clusters i e j, com a menor dij
     #definir novo cluster Ck com Clusters i e j e criar nos na altura dij/2
     #substituir clusters i e j por Ck
 
     #dado um novo cluster ck podemos calcular a sua distancia a todos os outros
 
-def print_tree:
+    final = Tree(clusters[0],clusters[1], clusters[0].data + clusters[1].data, d(clusters[0],clusters[1])/2)
+    print(final)
+
+def buildtreeheap(seq1):
+    heap = []
+    bitmap = {}
+
+    clusters = []
+
+    #Dar a cada sequencia o seu proprio cluster
+    #definir uma folha para cada sequencia e colocar na altura 0
+
+    for i in range(0, len(seq1)):
+        clusters.append(Tree(data = seq1[i], height = 0, weight = 0))
+
+    #enquanto ha mais que 2 clusters
+
+    
+
+    while (len(clusters) > 2):
+        #fazer tuplos de todos os clusters possíveis com distancia associada -> (dij, Tree(di,dj))
+        listadetuplos = [Tree(x,y,x.data+y.data,d(x,y),d(x,y)/2) for (x,y) in list(itertools.combinations(clusters, 2))]
+        #incluir tuplos na heap
+        #fazer, paralelamente, um dicionario com correspondencia de validade do tuplo; 'abcd' -> 'on'
+        for tree in listadetuplos:
+            heappush(heap,tree)
+            t = tree
+            bitmap[t.data] = "on"
+
+        newcluster = heappop(heap)
+
+        #fazer pop da heap ate sair um cujo data corresponda a 'on'
+
+        while(bitmap[newcluster.data] != "on"):
+            newcluster = heappop(heap)
+
+        print(newcluster)
+        clusters.append(newcluster)
+        clusters.remove(newcluster.left)
+        clusters.remove(newcluster.right)
+
+        #invalidar ('off') todos os elementos do dicionario cuja chave contenha um ou mais elementos do tuplo de cima
+        appears_on = [key for key in bitmap.keys() if bitmap[key] == "on"]
+
+        for l in newcluster.data:
+            for key in appears_on:
+                if l in key:
+                    bitmap[key] = "off"
+
+    final = Tree(clusters[0],clusters[1], clusters[0].data + clusters[1].data, d(clusters[0],clusters[1])/2)
+    print(final)
+
+
+
 
     
 
 if __name__ == '__main__':
-    seq1 = "AAPGE"
+    seq1 = "ACGCG"
 
-    buildtree(seq1)
+    buildtreeheap(seq1)
 
 
 
